0c47585b3bd0be8122b01cfe55ecfbad897c24de
GiorgioDordo <djole6789@gmail.com>
2024-08-07 16:36:50 +0200

    finished exercise without the bonuses

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ console.log('JS OK');
 const kmTodoField = document.getElementById('km');
 const ageField = document.getElementById('age');
 const calculateBtn = document.getElementById('calculate')
+const nameUserField = document.getElementById('firstname-lastname');
+
 const pricePerkm = 0.21;
 
 calculateBtn.addEventListener('click', function(e) {
     // no ricaricamento
     e.preventDefault();
 
+    const nameText = document.getElementById('name');
+    const nameValue = nameUserField.value;
+    nameText.innerHTML = nameValue;
+    console.log('nameValue', nameValue);
+
+    const typeTicket = document.getElementById('ticket-type');
     const fPriceMsg = document.getElementById('final-price');
-    const dscMsg = document.getElementById('message');
+    const dscMsg = document.getElementById('ticket-price'); 
+
     const kmTodoValue = parseInt(kmTodoField.value);
     const ageValue = ageField.value;
     console.log('kmTodoValue', kmTodoValue);
@@ -23,15 +32,18 @@ calculateBtn.addEventListener('click', function(e) {
     if (ageValue === 'adult') {
         fullPrice = basePrice;
         console.log('fullPrice', fullPrice);
-        dscMsg.innerHTML = `Il prezzo è: <strong>${fullPrice.toFixed(2)}€<strong>`;
+        dscMsg.innerHTML = `${fullPrice.toFixed(2)}€`; 
+        typeTicket.innerHTML = `STANDARD`;
     } else if (ageValue === 'underage') {
         fullPrice = basePrice * 0.8;
         console.log('fullPrice', fullPrice);
-        dscMsg.innerHTML = `Congratulazioni hai ricevuto uno sconto del 20%. Il prezzo è <strong>${fullPrice.toFixed(2)}€<strong>`;
+        dscMsg.innerHTML = `${fullPrice.toFixed(2)}€`;
+        typeTicket.innerHTML = `20% discount`;
     } else if (ageValue === 'over65') {
         fullPrice = basePrice * 0.6;
         console.log('fullPrice', fullPrice);
-        dscMsg.innerHTML = `Congratulazioni hai ricevuto uno sconto del 40%. Il prezzo è <strong>${fullPrice.toFixed(2)}€<strong>`;
+        dscMsg.innerHTML = `${fullPrice.toFixed(2)}€`;
+        typeTicket.innerHTML = `40% discount`;
     }
 
 }
